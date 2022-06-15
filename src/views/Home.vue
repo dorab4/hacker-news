25cfe4fb71bcbfd4a02c1c0bd784340a7e9fd33d
dorab4 <35685759+dorab4@users.noreply.github.com>
2022-06-15 21:04:13 +0200

    cleanup

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <HackerStories />
+  </div>
+</template>
+
+<script>
+import HackerStories from "@/components/HackerStories";
+export default {
+  name: "Home",
+  components: {
+    HackerStories,
+  },
+};
+</script>
